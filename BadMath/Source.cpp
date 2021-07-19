3fd93abad10bb2b7272e0c38ad9a9aeff57dc9d8
omermukhtarw9s9 <omersalwa1999@hotmail.com>
2021-07-18 23:09:05 -0400

    code has been updated

--- BadMath/Source.cpp
+++ BadMath/Source.cpp
@@ -1,19 +1,26 @@
+//Omer Mukhtar
+//Cis 1202
+// 18 june 2021
+
 #include <iostream>
 #include <iomanip>
 using namespace std;
 
 float average(int i1, int i2, int i3)
 {
-	return i1 + i2 + i3 / 3.0f;
+	return (i1 + i2 + i3) / 3.0f;
+	// add () for all the scores 
 }
 
 int main()
 {
-	int n1 =0 , n2 = 0, n3 = 0;
+	int n1 = 0, n2 = 0, n3 = 0;
 	cout << "This program calculates the average of three numbers." << endl;
 	cout << "First number: "; cin >> n1;
 	cout << "Second number: "; cin >> n2;
-	cout << "Third number: "; cin >> n2;
+	cout << "Third number: "; cin >> n3;
+
+	// cout third number cin >> n2 must be changed to n3
 
 	float a = average(n1, n2, n3);
 
